1537e1f364ee07d456cde40c06d9f25a0e562694
awwmicky <michael_fred_alvarez@yahoo.com>
2020-02-23 11:24:36 -0800

    front-end react setup

---
+++ client/src/components/CompOne/index.js
@@ -0,0 +1,14 @@
+import React from 'react' // , { useState }
+import './comp-one.css'
+
+
+
+export default function CompOne () {
+
+  return (
+    <div className="App">
+      <h1 className="comp-one">COMP ONE</h1>
+    </div>
+  );
+
+}
---
+++ client/src/index.js
@@ -0,0 +1,28 @@
+// import * as serviceWorker from './serviceWorker'
+import ReactDOM from 'react-dom'
+import React from 'react'
+import './index.css'
+// import ___ from './components/___/'
+import CompOne from './components/CompOne/'
+import CompTwo from './components/CompTwo/'
+
+
+
+function App () {
+
+  return (
+    <div className="App">
+      <h1>Title</h1>
+
+      <CompOne />
+      <CompTwo />
+    </div>
+  );
+
+}
+
+
+
+const root = document.getElementById('root');
+ReactDOM.render( <App /> , root )
+// serviceWorker.unregister();
