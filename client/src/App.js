20a28c8e30d46fb8f8fa4b4af4c6b4a054c307fa
Jim <jimmybnguyen@yahoo.com>
2020-02-28 16:25:02 -0800

    section created

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,21 @@
-import React from 'react'
-import './reset.css'
-import './app.css'
+import React from "react";
+import "./reset.css";
+import "./app.css";
 // import ___ from './components/___/'
-import Header from './components/Header/Header.js'
-import Main from './components/Main/Main.js'
+import Header from "./components/Header/Header.js";
+import Main from "./components/Main/Main.js";
+import Banner from "./components/Banner/Banner.js";
+
 // import axios from 'axios';
 
-
-
-function App () {
+function App() {
   return (
     <div className="App">
       <Header />
       <Main />
+      <Banner />
     </div>
   );
 }
 
-
-
-export default App
+export default App;
