616344adb10ab3cc88624255667aa8505d154b73
awwmicky <michael_fred_alvarez@yahoo.com>
2020-02-28 16:17:45 -0800

    client routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,20 @@
+import { 
+  BrowserRouter as Router, 
+  Route , Switch
+} from 'react-router-dom'
 import React from 'react'
+
 import './reset.css'
 import './app.css'
+
 // import ___ from './components/___/'
 import Header from './components/Header/Header.js'
-import Main from './components/Main/Main.js'
+// import Main from './components/Main/Main.js'
+
+import HomePage from './views/HomePage/HomePage.js'
+import MainPage from './views/MainPage/MainPage.js'
+// import LandingPage from './views/LandingPage/LandingPage.js'
+
 // import axios from 'axios';
 
 
@@ -11,8 +22,24 @@ import Main from './components/Main/Main.js'
 function App () {
   return (
     <div className="App">
-      <Header />
-      <Main />
+      <Router>
+        <Header />
+
+        <Switch>
+          <Route exact path="/">
+            <HomePage />
+          </Route>
+
+          <Route exact path="/main">
+            <MainPage />
+          </Route>
+
+          {/* <Route exact path="/landing"> */}
+            {/* <LandingPage /> */}
+          {/* </Route> */}
+        </Switch>
+
+      </Router>
     </div>
   );
 }
--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,6 +1,7 @@
-import React from 'react'
-// , { useState } 
-import './header.css'
+import React from 'react' // , { useState } 
+import './Header.css'
+import Logo from './Logo/Logo.js'
+import NavBar from './NavBar/NavBar.js'
 
 
 
@@ -9,39 +10,8 @@ function Header () {
   return (
     <header>
       <div className="container-wrapper">
-        <div className="header-logo">
-          <img src="/assets/images/favicon.png" alt="logo" className="img-logo" />
-          <h1 className="title-lg">
-            Pooch Match
-          </h1>
-        </div>
-        <nav className="navbar">
-          <ul className="nav-opts">
-            <li>
-              <a href="/">
-                Home
-              </a>
-            </li>
-            <li>
-              <a href="/">
-                View All
-              </a>
-            </li>
-            <li>
-              <a href="/">
-                Account
-              </a>
-            </li>
-          </ul>
-        </nav>
-        {/* <div className="menu-bar">
-          <a href="#" className="nav-btn">
-            <i className="fas fa-bars"></i>
-          </a>
-          <a href="#" className="exit-btn">
-            <i className="fas fa-times"></i>
-          </a>
-        </div> */}
+        <Logo />
+        <NavBar />
       </div>
     </header>
   );
@@ -51,3 +21,16 @@ function Header () {
 
 
 export default Header
+
+
+
+/* 
+        <div className="menu-bar">
+          <a href="#" className="nav-btn">
+            <i className="fas fa-bars"></i>
+          </a>
+          <a href="#" className="exit-btn">
+            <i className="fas fa-times"></i>
+          </a>
+        </div> 
+*/
