6841b49fc48588b106b1fb7b4c20f23cfbd842ec
SleepyJen <jennifer.ni0924@gmail.com>
2020-03-01 19:40:51 -0800

    added concurrently and if production stage in server

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 8000;
 const Colors = require('colors');
+const path = require('path');
 
 const mongoose = require('mongoose');
 const MONGO_URI =
@@ -16,12 +17,16 @@ mongoose.connect(MONGO_URI, {
     console.log('------------------------------'.rainbow);
 }).catch(err => console.log(err));
 
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'));
-}
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
 
 app.listen(PORT, () => {
     console.log('\n------------------------------'.rainbow);
