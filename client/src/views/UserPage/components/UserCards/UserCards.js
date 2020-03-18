50d1b01da7ae16ffbdb2df0bd74b2f992c61cdcb
SleepyJen <jennifer.ni0924@gmail.com>
2020-03-17 21:23:52 -0700

    created user card for each person in the area

--- client/src/views/UserPage/components/UserCards/UserCards.js
+++ client/src/views/UserPage/components/UserCards/UserCards.js
@@ -1,8 +1,9 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import Interests from '../Interests/Interests';
 
 function UserCards(props) {
-    const [value, modifier] = useState({ people: [] });
+    const [value, modifier] = useState({ people: [], imgs: [] });
 
     axios.get(`/user/getByCity/${props.City}`).then(result => {
         let ppl = result.data;
@@ -11,29 +12,28 @@ function UserCards(props) {
         for (let i = 0; i < ppl.length; i++) {
             if (ppl[i]._id !== props._id) {
                 data.push(ppl[i]);
+                console.log(ppl[i]);
                 if (ppl[i].imgs) {
                     axios.get(`/addImage/${ppl[i].imgs}`).then(res => {
+                        imgs.push(res.data.data);
                     });
                 }
             }
         }
-        modifier({ people: data });
+        modifier({ people: data, imgs: imgs });
     });
     return (
         <div >
-
             {value.people.map((person, index) => (
-                <div className="card">
-                    <img className="card-img-top" src={person.imgs} alt="Card image cap" />
+                <div className="card" key={index}>
+                    <img className="card-img-top" src={value.imgs[index]} alt="profile pic" />
                     <div className="card-body">
-                        <h5 className="card-title">Card title</h5>
-                        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        <a href="#" className="btn btn-primary">Go somewhere</a>
+                        <h5 className="card-title">{person.firstName} {person.lastName}</h5>
+                        <p className="card-text"><Interests _id={person._id} /></p>
+                        <a href="#" className="btn btn-primary">View website</a>
                     </div>
                 </div>
             ))}
-            {/* 
-             */}
         </div>
     )
 }
