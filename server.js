3e542f54882e5cc3f7973d66aa49ac5f7aad55ed
SleepyJen <jennifer.ni0924@gmail.com>
2020-03-21 12:42:13 -0700

    pushing to heroku again

--- server.js
+++ server.js
@@ -45,7 +45,7 @@ app.use('/comments', commentsRoutes);
 
 app.use('/uploads', express.static('./uploads'));
 
-if (true) {
+if (process.env.NODE_ENV === 'production') {
     app.use(express.static('client/build'));
     app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
