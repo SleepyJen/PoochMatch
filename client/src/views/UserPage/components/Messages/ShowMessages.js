6ced1dd5c05d2cedd5f8d0bef946cae3dac10e09
SleepyJen <jennifer.ni0924@gmail.com>
2020-03-20 23:07:04 -0700

    created a way to comment

--- client/src/views/UserPage/components/Messages/ShowMessages.js
+++ client/src/views/UserPage/components/Messages/ShowMessages.js
@@ -12,30 +12,53 @@ function ShowMessages(props) {
         let data = [];
         for (let i = 0; i < props.comments.length; i++) {
             let info = {
-                userId: ""
+                userId: "",
+                commentId: ""
             }
             axios.get(`/comments/${props.comments[i]}`).then(result => {
                 for (let j = 0; j < result.data.userId.length; j++) {
                     if (result.data.userId[j] !== props.userId) {
                         info['userId'] = result.data.userId[j];
+                        info["commentId"] = props.comments[i];
                     }
                 }
                 data.push(info);
                 modifyUser({ id: data });
             });
         }
-    }, [props]);
+    }, [props, chatButton]);
 
     async function clicked(name) {
-        console.log(name);
         const urlQuerries = new URLSearchParams(window.location.search);
         const userId = urlQuerries.get('user_id');
         await axios.get(`/user/getById/${userId}`).then(result => {
             modifyComments({ comment: result.data.comments });
         });
-        modifyButton({ ['id']: name });
+        modifyButton({ id: name, comment: chatButton.comment });
     }
 
+    function handleMessage(event) {
+        const value = event.target.value;
+        modifyButton({ id: chatButton.id, comment: value });
+    }
+
+    async function submit(e) {
+        e.preventDefault();
+        console.log(chatButton);
+        console.log(user.id[0].commentId);
+        const urlQuerries = new URLSearchParams(window.location.search);
+        const userId = urlQuerries.get('user_id');
+        let data = {
+            from: userId,
+            comment: chatButton.comment
+        }
+        console.log(data);
+        await axios.post(`/comments/addComment/${user.id[0].commentId}`, {
+            comment: data
+        }).then(result => {
+            modifyButton({ id: chatButton.id, comment: chatButton.comment });
+        });
+    }
 
     return (
         <div>
@@ -57,8 +80,8 @@ function ShowMessages(props) {
                         </div>
                         <div className="chatInput">
                             <label>Message: </label>
-                            <input type="textarea"></input>
-                            <button type="submit" className="btn btn-primary">Send</button>
+                            <input onChange={handleMessage} type="textarea"></input>
+                            <button onClick={submit} type="submit" className="btn btn-primary">Send</button>
                         </div>
                     </fieldset>
                 </form>
